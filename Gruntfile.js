1a8c8a2866864ce7dc51877193a4d6be93de8c11
Reid Ransom <reid@reidransom.com>
2013-11-11 14:03:24 -0800

    clean up

--- Gruntfile.js
+++ Gruntfile.js
@@ -82,7 +82,6 @@ module.exports = function(grunt) {
     grunt.loadTasks('tasks');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-nodeunit');
     grunt.loadNpmTasks('grunt-mocha-test');
 
     grunt.registerTask('test', ['clean', 'mochaTest']);
